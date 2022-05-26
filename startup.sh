3e8ad3b29225e84c8f8ff7f99e5eceb8dae0c5c3
luolongfei <luolongf@gmail.com>
2022-05-26 09:44:38 +0800

    update

---
+++ startup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+envsubst < /root/config.json.tp > /root/config.json
+envsubst '\$PORT' < /root/nginx.template.conf > /root/nginx.conf
+
+/usr/bin/v2ray -config /root/config.json & nginx -c /root/nginx.conf -g 'daemon off;'
